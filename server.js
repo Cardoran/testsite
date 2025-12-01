52a2ad73ebf6087d03dccfc2374801b636da7b93
Cardoran <ccardoran@gmail.com>
2025-12-01 22:06:42 +0100

    changed port from 3000 to 80

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 
 // Serve static files (HTML, CSS, JS)
 app.use(express.static('public'));
