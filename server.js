28a2b2978bce3269b3597ef22dc2d88068d3e16c
Cardoran <ccardoran@gmail.com>
2025-12-04 18:27:15 +0100

    try simpler version

--- server.js
+++ server.js
@@ -53,9 +53,9 @@ app.get('/api/graphdata', (req, res) => {
       labels: xLabels,
       datasets: [
         labels.map((element,index) => ({label: element,
-            data: latestData["unix_seconds"].map(
-                (element, index) => [element,latestData[element][index]]
-            ),
+            // data: latestData["unix_seconds"].map(
+            //     (element, index) => [element,latestData[element][index]]
+            // ),
             borderColor: colors[index],
             backgroundColor: colors[index],
             fill: true})
