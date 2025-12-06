93abec6fe53fe9bb29c771c4de187777cdc135d4
Cardoran <ccardoran@gmail.com>
2025-12-07 00:08:32 +0100

    use bodyParser

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 80;
 
 // Serve static files (e.g., HTML, CSS, JS)
 app.use(express.static('public'));
-app.use(express.json());
+app.use(express.bodyParser());
 
 
 // Global variable to store the latest data
