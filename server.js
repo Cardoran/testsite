ceb208c819f294d91666ec82ac5d6bedf3df028e
Cardoran <ccardoran@gmail.com>
2025-12-07 00:11:53 +0100

    use href

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const PORT = 80;
 
 // Serve static files (e.g., HTML, CSS, JS)
 app.use(express.static('public'));
-app.use(express.json({type: '*/*'}));
 
 
 // Global variable to store the latest data
