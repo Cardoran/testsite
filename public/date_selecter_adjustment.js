efcbf687d3b366b55c68f51c129b06fe4541971b
Cardoran <ccardoran@gmail.com>
2025-12-18 22:01:09 +0100

    set focus on enddate when choosing a later startdate

--- public/date_selecter_adjustment.js
+++ public/date_selecter_adjustment.js
@@ -22,6 +22,7 @@ document.getElementById('start').addEventListener('change', function() {
     const endDateInput = document.getElementById('end');
     if (endDateInput.value && endDateInput.value < startDate) {
       endDateInput.value = '';
+      endDateInput.focus();
     }
   });
 
