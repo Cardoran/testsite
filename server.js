a2489cf3bb0d09a598979ad50d083221469e25cf
Cardoran <ccardoran@gmail.com>
2026-08-25 00:24:57 +0200

    add another error catcher

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.get('/updateLatestData', async (req, res) => {
         console.log(emissions);
         console.log("Data updated successfully.");
     } catch (error) {
+      c
         console.error("Failed to update data:", error.message);
     }
   res.json([]);
